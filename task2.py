4c5eab9d6f67d1f6c98ffcd33d984a434e192967
Sumit <sumitdhiman181@gmail.com>
2026-03-06 17:09:17 +0530

    Implement square root, logarithm, and sine functions

---
+++ task2.py
@@ -0,0 +1,15 @@
+import  math
+num = float(input("enter your number : "))
+def sqrt(n):
+    a = (math.sqrt(n))
+    print(f"square root: {a}")
+sqrt(num)
+
+def log(m):
+    b = (math.log(m))
+    print(f"logarithm: {b}")
+log(num)
+def sine(p):
+    c = (math.sin(p))
+    print(f"sine: {p}")
+sine(num)
